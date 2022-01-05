bb20ab359219cf183b26d0f62e83e410d97138e1
Matija Skrletović <skrlama@gmail.com>
2022-01-05 22:05:06 +0100

    Solution to Day1 part 2 advent of code

--- Number Spiral/src/adventcalendar/Day01part2.java
+++ Number Spiral/src/adventcalendar/Day01part2.java
@@ -4,31 +4,42 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Day01 {
-	
+public class Day01part2 {
+
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner scanner = new Scanner(new File("/home/skrla/Downloads/input.txt"));
-		int x = Integer.MIN_VALUE;
+		int x = 0;
 		int y;
 		int times = 0;
 		int count = 0;
+		int a = 0;
+		int b = 0;
+		int c = 0;
 		
-		while (scanner.hasNextInt()) {
+		while(scanner.hasNextInt()) {
 			y = scanner.nextInt();
 			if (times == 0) {
-				x = y;
+				a = y;
 				times++;
-			} else if (y > x) {
-				count++;
-				x = y;
-				
+			} else if (times == 1) {
+				b = y;
+				times++;
+			} else if (times == 2) {
+				c = y;
+				times++;
 			} else {
-				x = y;
+				times = a + b + c;
+				if (times > x) {
+					count++;
+				}
+				a = b;
+				b = c;
+				c = y;
+				x = times;
 			}
-			
-		}
-	
+			}
+		
 		System.out.println(count);
-	}
-	
+		
+		}
 }
